a7db56802ac5fe40cf731daf3e116941edc626ff
CanqueHanzel <153728179+HanzelBCanque@users.noreply.github.com>
2024-08-21 17:57:52 +0800

    Add files via upload

---
+++ Chapter 1/SwingAlert.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class SwingAlert {
+ public static void main (String [] args)
+   {
+      JOptionPane.showMessageDialog(null, "Hello World!");
+ 
+   }
+ 
+} 
+
