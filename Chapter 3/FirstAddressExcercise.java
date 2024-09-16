96bc7cac11d3ec15f612fd23b55b81a6785312c4
CanqueHanzel <153728179+HanzelBCanque@users.noreply.github.com>
2024-09-16 20:35:17 +0800

    Add files via upload

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,15 @@
+public class FirstAddressExcercise{
+
+public static void main(String[]args){
+
+   System.out.println("First Java application");
+   displayAddress();
+}
+
+public static void displayAddress(){
+   System.out.println("Samsung Electronics America");
+   System.out.println("85 Challenger Road");
+   System.out.println("Ridgefield Park,NJ 07660, USA");
+   System.out.println("Company: Samsung Electronics");
+         }
+}
